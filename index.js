65e3d3c1f4aa23333a55119315e399022bf46f07
Bernardo Ribeiro <bernardosr25@gmail.com>
2021-11-03 10:30:15 -0300

    feito a criação de autenticação para os funcionários

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./Conta/ContaCorrente.js";
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+import {Gerente} from "./funcionario/Gerente.js";
+import {Diretor} from "./funcionario/Diretor.js";
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretor("bernardo",2500, 123456464);
+diretor.cadastrarSenha("123456")
+const gerente = new Gerente("bernardo 2", 5000, 12315646523)
+gerente.cadastrarSenha("123");
 
-const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
 
-console.log(contaSalario);
+
+console.log(gerenteEstaLogado, diretorEstaLogado);
