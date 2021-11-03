dacdfca66b6f7451f87e654fa3dd174eb0d0f625
Bernardo Ribeiro <bernardosr25@gmail.com>
2021-11-03 09:52:24 -0300

    Criado a classe Conta Salário e colocado o metodo sacar como abstrato

--- Conta.js
+++ Conta.js
@@ -27,8 +27,7 @@ export class Conta {
     }
 
     sacar(valor) {
-        let taxa = 1
-        return this._sacar(valor, taxa);
+        throw new Error("Favor implementar o metodo sacar")
     }
 
     _sacar(valor, taxa){
